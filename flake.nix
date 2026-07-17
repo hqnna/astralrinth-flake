02d2f147e5a7f96ec508ea82fdf302fdcbb5da64
hanna <me@hanna.lol>
2026-07-16 21:39:43 -0400

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts/main";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem = args@{ pkgs, ... }: {
+        packages.default = import ./pkgs/default.nix args;
+      };
+    };
+}
---
+++ pkgs/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+let
+  pname = "astralrinth";
+  version = "0.14.801";
+
+  src = pkgs.fetchurl {
+    url = "https://git.astralium.su/didirus/AstralRinth/releases/download/AR-${version}/AstralRinth%20App_${version}_amd64.AppImage";
+    hash = "sha256-aBjhHMdkIP0wlHP3jtCGclhxI/jF2s9WH9KNeZnq/w8=";
+    name = "AstralRinth_${version}_amd64.AppImage";
+  };
+
+  image = pkgs.appimageTools.extractType1 { inherit pname src; };
+in
+pkgs.appimageTools.wrapType2 rec {
+  inherit pname version src;
+
+  meta = with pkgs.lib; {
+    description = "A modern launcher based on Modrinth.";
+    homepage = "https://git.astralium.su/didirus/AstralRinth";
+    downloadPage = "https://git.astralium.su/didirus/AstralRinth/releases";
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    maintainers = with maintainers; [ hanna ];
+    mainProgram = "AstralRinthApp";
+    platforms = [ "x86_64-linux" ];
+    license = licenses.mit;
+  };
+}
