cebe8fd83da1f668c5081e3f0a0696f6b957cb4f
hanna <me@hanna.lol>
2026-07-16 22:44:50 -0400

    fix: just remove all bundled libs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -15,8 +15,7 @@ let
   patched = pkgs.runCommand "${pname}-patched" { } ''
     cp -r ${image} $out
     chmod -R u+w $out
-    rm -f $out/usr/lib/libwayland-*.so*
-    rm -f $out/usr/lib/libgst*.so*
+    rm -rf $out/usr/lib
   '';
 in
 pkgs.appimageTools.wrapAppImage rec {
@@ -24,18 +23,22 @@ pkgs.appimageTools.wrapAppImage rec {
   
   src = patched;
 
+  extraPkgs = pkgs: with pkgs; [
+    webkitgtk_4_1
+    libsoup_3
+    libayatana-appindicator
+  ] ++ (with pkgs.gst_all_1; [
+    gstreamer
+    gst-plugins-base
+    gst-plugins-good
+  ]);
+
   profile = ''
     unset GST_PLUGIN_PATH GST_PLUGIN_PATH_1_0 GST_PLUGIN_SYSTEM_PATH GST_PLUGIN_SYSTEM_PATH_1_0
     export GST_PLUGIN_SYSTEM_PATH_1_0=/usr/lib/gstreamer-1.0
     export GST_PLUGIN_SCANNER=/usr/libexec/gstreamer-1.0/gst-plugin-scanner
     export GST_REGISTRY_1_0="''${XDG_RUNTIME_DIR:-/tmp}/${pname}-gst-registry.bin"
   '';
-
-  extraPkgs = pkgs: with pkgs.gst_all_1; [
-    gstreamer
-    gst-plugins-base
-    gst-plugins-good
-  ];
 
   extraInstallCommands = ''
     cp -r "${image}/usr/share" "$out/share"
